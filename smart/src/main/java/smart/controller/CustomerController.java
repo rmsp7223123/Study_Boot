212836c99e426c1171536699be0f081a85bfd5c1
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-21 17:22:39 +0900

    ++

--- smart/src/main/java/smart/controller/CustomerController.java
+++ smart/src/main/java/smart/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package smart.controller;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import groovyjarjarantlr4.v4.Tool.Option;
+import smart.entity.hanul.Customer;
 import smart.repository.hanul.CustomerRepository;
 
 @Controller
@@ -15,19 +19,21 @@ import smart.repository.hanul.CustomerRepository;
 public class CustomerController {
 	@Autowired
 	private CustomerRepository repository;
-	
+
 	@RequestMapping("/info/{id}")
 	public String info(@PathVariable int id, Model model) {
-		
-		return"customer/info";
+		Optional<Customer> optional = repository.findById(id);
+		Customer vo = optional.isPresent() ? optional.get() : null;
+		model.addAttribute("vo", vo);
+		return "customer/info";
 	}
-	
+
 	@RequestMapping("/info")
 	public String info(Model model, int id) {
-		
-		return"customer/info";
+		Customer vo = repository.findById(id).orElse(null);
+		model.addAttribute("vo", vo);
+		return "customer/info";
 	}
-	
 
 	@RequestMapping("/list")
 	public String list(Model model, HttpSession session) {
